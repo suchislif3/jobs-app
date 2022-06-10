5a92dbc92e16422a9ccb4659275b532846fe02f9
Szabolcs Devecseri <sz.devecseri@gmail.com>
2022-06-10 17:29:00 +0200

    fixed User model

--- server/models/User.js
+++ server/models/User.js
@@ -16,7 +16,7 @@ const UserSchema = new mongoose.Schema({
     ],
     unique: true,
   },
-  name: {
+  password: {
     type: String,
     required: [true, "Please provide password"],
     minLength: 6,
