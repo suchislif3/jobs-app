f0cea8190a7628fa6a557d0f01e70700d90a6ddd
Szabolcs Devecseri <sz.devecseri@gmail.com>
2022-06-10 17:27:31 +0200

    added User model

---
+++ server/models/User.js
@@ -0,0 +1,28 @@
+import mongoose from "mongoose";
+
+const UserSchema = new mongoose.Schema({
+  name: {
+    type: String,
+    required: [true, "Please provide name"],
+    minLength: 3,
+    maxLength: 50,
+  },
+  email: {
+    type: String,
+    required: [true, "Please provide email"],
+    match: [
+      /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/,
+      "Please provide a valid email",
+    ],
+    unique: true,
+  },
+  name: {
+    type: String,
+    required: [true, "Please provide password"],
+    minLength: 6,
+  },
+});
+
+const User = mongoose.model("User", UserSchema);
+
+export default User;
