3eaf1629c832f969c32f198d80eebec3033dc6f7
Szabolcs Devecseri <sz.devecseri@gmail.com>
2022-06-14 20:47:01 +0200

    navbar init style

--- client/src/components/Navbar.js
+++ client/src/components/Navbar.js
@@ -1,10 +1,11 @@
 import React from "react";
+import { Wrapper } from "../styles/Navbar.styles";
 
 const Navbar = () => {
   return (
-    <>
+    <Wrapper>
       <div>Navbar</div>
-    </>
+    </Wrapper>
   );
 };
 
---
+++ client/src/styles/Navbar.styles.js
@@ -0,0 +1,8 @@
+import styled from "styled-components";
+
+export const Wrapper = styled.nav`
+  height: 6rem;
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+`;
