4b931949df27f1f6364c436cb4b91908f43301f5
Szabolcs Devecseri <sz.devecseri@gmail.com>
2022-06-22 11:24:38 +0200

    navbar functional

--- client/src/components/Navbar.js
+++ client/src/components/Navbar.js
@@ -1,9 +1,23 @@
+import { Link } from "react-router-dom";
+import { useGlobalContext } from "../context/appContext";
 import { Wrapper } from "../styles/Navbar.styles";
+import { FaRegUserCircle } from "react-icons/fa";
+import { FiLogOut } from "react-icons/fi";
 
 const Navbar = () => {
+  const { user, logout } = useGlobalContext();
+
   return (
     <Wrapper>
-      <div>Navbar</div>
+      <Link to="/">JobsApp</Link>
+      {user && (
+        <div>
+          <FaRegUserCircle /> {user}
+          <button onClick={logout}>
+            Logout <FiLogOut />
+          </button>
+        </div>
+      )}
     </Wrapper>
   );
 };
