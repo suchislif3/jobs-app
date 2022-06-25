efc138ac7e67570184b7cda1a61ca73daed5d8a1
Szabolcs Devecseri <sz.devecseri@gmail.com>
2022-06-25 21:26:18 +0200

    centered loader

--- client/src/styles/Jobs.styles.js
+++ client/src/styles/Jobs.styles.js
@@ -2,6 +2,9 @@ import styled from "styled-components";
 
 export const Wrapper = styled.div`
   width: 100%;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
   h3 {
     margin-bottom: 2%;
   }
